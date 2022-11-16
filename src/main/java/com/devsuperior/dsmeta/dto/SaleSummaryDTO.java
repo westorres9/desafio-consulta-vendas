afedded27097ee2b4847401aef1d128a1d4505fa
westorres9 <wester.torres9@gmail.com>
2022-11-16 12:39:44 -0300

    SaleReportDTO, SaleSummaryDTO

---
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleSummaryDTO.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsmeta.dto;
+
+import com.devsuperior.dsmeta.projections.SalesSumaryProjection;
+
+public class SaleSummaryDTO {
+
+    private String sellerName;
+    private Double total;
+
+    public SaleSummaryDTO(){
+    }
+
+    public SaleSummaryDTO(String sellerName, Double total) {
+        this.sellerName = sellerName;
+        this.total = total;
+    }
+
+    public SaleSummaryDTO(SalesSumaryProjection projection) {
+        this.sellerName = projection.getSellerName();
+        this.total = projection.getTotal();
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+}
